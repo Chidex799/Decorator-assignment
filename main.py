74c441bd0efab71ee45145688e36be6fc08de8de
Chidex799 <87649692+Chidex799@users.noreply.github.com>
2021-07-29 16:25:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 def write_file(sample_func):
     def execute():
         result_2 = sample_func()
-        with open(result_2, "w+") as my_file:
+        with open(result_2,"w+") as my_file:
             print(my_file.write(result_2))
-
     return execute
-
 
 def create_file(sample_fuc):
     def execute():
         result = sample_fuc()
         with open(result, "w") as my_file:
+            print(my_file.write("file_created"))
         return result
-
     return execute
-
-
 @write_file
 @create_file
 def filesTest():
     return "Today is a good day.txt"
-
 
 filesTest()
